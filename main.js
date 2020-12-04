32faced204232bf1de0b47e6e3a138a7a06189e1
Sarthak shukla <sarthak1shukla@gmail.com>
2020-12-04 10:59:49 +0530

    add changes

--- main.js
+++ main.js
@@ -11,43 +11,36 @@ function leadingZero(time){
     }
     return time;
 }
-
 function timerstart(){
     let currenttime = leadingZero(timer[0])+":"+leadingZero(timer[1])+":"+timer[2];
     clock.innerHTML = currenttime;
-    timer[3]++;
+    timer[3]++; 
+
     timer[0] = Math.floor((timer[3]/100)/60);
     timer[1] = Math.floor((timer[3]/100) - (timer[0]*60));
     timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
 }
-
-
-
 function start(){
     let textlenght = txtar.value.length;
     if(textlenght === 0 && !timerRunning){
         timerRunning = true;
         interval = setInterval(timerstart,10);
     }
-    // console.log(textlenght);
 }
 function spellcheck(){
     let textEnter = txtar.value;
     let orgtextsub = orgtext.substring(0,textEnter.length);
     if(textEnter == orgtext ){
         clearInterval(interval);
-        txtar.style.borderColor="green";
+        txtar.style.borderColor="#3d985d";
     }
     else if(textEnter == orgtextsub)
     {
-        txtar.style.borderColor="blue";
+        txtar.style.borderColor="#7769ef";
     }
     else{
-        txtar.style.borderColor="red";
+        txtar.style.borderColor="#ea3939";
     }
-    // console.log(textEnter);
-    // console.log(orgtextsub);
-    // console.log(orgtext);
 }
 function reset(){
     clearInterval(interval);
